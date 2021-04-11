f8bab3ead9d6a55359508fa5f00a068d69f7a056
taurusilver7 <prasad20697@gmail.com>
2021-04-11 22:23:04 +0530

    search route created

---
+++ pages/search.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Header from "./Header";
+
+const Search = () => {
+  return (
+    <div>
+      <Head>
+        <title>Search result</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header></Header>
+    </div>
+  );
+};
+
+export default Search;
