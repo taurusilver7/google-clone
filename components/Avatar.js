70122364c058438437e2ba1c4a49002cc9cffa49
taurusilver7 <prasad20697@gmail.com>
2021-04-11 22:23:57 +0530

    custom component created for avatar

---
+++ components/Avatar.js
@@ -0,0 +1,12 @@
+const Avatar = ({ url }) => {
+  return (
+    <img
+      loading="lazy"
+      className="h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110"
+      src={url}
+      alt="profile pic"
+    />
+  );
+};
+
+export default Avatar;
