f172eaa7857c29da44d18a22ee3583663b919de2
taurusilver7 <prasad20697@gmail.com>
2021-04-12 21:54:49 +0530

    api keys added

---
+++ keys.js
@@ -0,0 +1,6 @@
+// API key -- https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = "AIzaSyA7jbZmSiV0aPXk-nTw-E6rTQSOnWxOfR0";
+
+// Context key -- https://cse.google.com/cse/create/new
+// <script async src="https://cse.google.com/cse.js?cx=830abcb9ded145d07"></script>
+export const CONTEXT_KEY = "830abcb9ded145d07";
