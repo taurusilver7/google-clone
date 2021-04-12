d7dc298dc0201668a3b1c709e97d48a92bff08fe
taurusilver7 <prasad20697@gmail.com>
2021-04-12 21:59:03 +0530

    search page styled & upto-date

--- pages/search.js
+++ pages/search.js
@@ -1,16 +1,43 @@
 import Head from "next/head";
-import Header from "./Header";
+import { useRouter } from "next/router";
+import Header from "../components/Header";
+import SearchResults from "../components/SearchResults";
+import { API_KEY, CONTEXT_KEY } from "../keys";
+import Response from "../Response";
 
-const Search = () => {
+const Search = ({ results }) => {
+  const router = useRouter();
+  console.log(results);
   return (
     <div>
       <Head>
-        <title>Search result</title>
+        <title>{router.query.term} - Search result</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header></Header>
+      <Header />
+      {/* Search result parsed from the server */}
+      <SearchResults results={results} />
     </div>
   );
 };
 
 export default Search;
+
+export async function getServerSideProps(context) {
+  const useDummyData = false;
+  // pagination
+  const startIndex = context.query.start || "0";
+
+  const data = useDummyData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+      ).then((res) => res.json());
+
+  // after the SERVER-SIDE RENDERING,.. Pass the result to the client
+  return {
+    props: {
+      results: data,
+    },
+  };
+}
